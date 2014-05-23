ce2ca0d8e06fc69c2064abef55ff9b66aafa0593
Zak B. Elep <zak.elep@yoyo.ph>
2014-05-23 13:00:14 +0800

    Add basic caching on $resource requests
    Save a few bytes while we're at it :)

--- public/nytfeedfun/services.js
+++ public/nytfeedfun/services.js
@@ -9,6 +9,7 @@ angular.module('NYTFeedFun.services', ['ngResource'])
 
   .factory('Feeds', ['$resource', function($resource) {
     return $resource('nytfeedfun/feeds/:feedId.json', {}, {
-      query: {method:'GET', params:{feedId:'index'}, isArray:true}
+      get: {cache:true},
+      query: {method:'GET', params:{feedId:'index'}, isArray:true, cache:true}
     });
   }]);
