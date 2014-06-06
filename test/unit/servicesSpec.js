6d635e594c0ff2fd437f800c621de2cdeddafec0
Zak B. Elep <zak.elep@yoyo.ph>
2014-06-07 01:03:41 +0800

    Add JavaScript tests
    These are basic tests, based on angular-seed and angular-phonecat.

---
+++ test/unit/servicesSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/* jasmine specs for services go here */
+
+describe('service', function() {
+  beforeEach(module('NYTFeedFun.services'));
+
+  describe('Search', function() {
+    it('should return empty Search terms', inject(function(Search) {
+      expect(Search.terms).toBe('');
+    }));
+  });
+});
